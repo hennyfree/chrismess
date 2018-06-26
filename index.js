4057bca64ca4ec254b4abd98af1bfc8c1c209844
Henok <b.h.tesfay@gmail.com>
2018-06-26 15:08:59 -0400

    change button type

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-hkhjkj
+const form = document.querySelector('form#flickForm')
+
+const changeHeading = function(ev) {
+    
+    ev.preventdefault()
+    const f = ev.target
+
+
+    const flickName = f.flickName.value
+    const item = document.createElement('li')
+    item.textContent = flickName
+
+    const list = document.querySelector('#flicks')
+    list.appendChild(item)
+     
+    f.reset ()
+}
+
+form.addEventListener('submit', changeHeading)
