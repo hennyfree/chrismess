5f421e0068eb90bb5b630d642a470eef4600a7a9
Henok <b.h.tesfay@gmail.com>
2018-06-27 09:07:59 -0400

    the const and the form

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const form = document.querySelector('form#flickForm')
+const f4rm = document.querySelector('form#flickFormer')
 
-const changeHeading = function(ev) {
+const changeName = function(ev) {
     
-    ev.preventdefault()
+    ev.preventDefault()
     const f = ev.target
 
 
@@ -16,4 +17,19 @@ const changeHeading = function(ev) {
     f.reset ()
 }
 
-form.addEventListener('submit', changeHeading)
+const changeMovie = function(ev) {
+    ev.preventDefault()
+    const f = ev.target
+
+    const flickName = f.flickName.value
+    const item = document.createElement('li')
+    item.textContent = flickName
+
+    const list = document.querySelector('#flicks2')
+    list.appendChild(item)
+    f.reset ()
+}
+
+
+form.addEventListener('submit', changeName)
+f4rm.addEventListener('submit', changeMovie)
