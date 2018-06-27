25927cc1c0f51ee439e1ce7a2d13612263d80026
Henok <b.h.tesfay@gmail.com>
2018-06-27 16:38:33 -0400

    change the const and edit create list

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const form = document.querySelector('form#flickForm')
-const f4rm = document.querySelector('form#flickFormer')
 
-const changeName = function(ev) {
-    
+
+const handleSubmit= function(ev) {
     ev.preventDefault()
     const f = ev.target
 
 
     const flickName = f.flickName.value
-    const item = document.createElement('li')
-    item.textContent = flickName
+    const flickSpan = document.createElement('span')
+    flickSpan.textContent = flickName
+
+const movieName = f.movieName.value
+const chrisSpan = document.createElement('span')
+chrisSpan.textContent = chrisName
 
     const list = document.querySelector('#flicks')
     list.appendChild(item)
@@ -27,7 +30,9 @@ const changeMovie = function(ev) {
 
     const list = document.querySelector('#flicks2')
     list.appendChild(item)
+
     f.reset ()
+    fi.flickName.focus()
 }
 
 
